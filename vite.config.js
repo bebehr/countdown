cc9bbb656a48f0b357fd02c549b84ba3a8ffb71b
bebehr <behr.benny@gmail.com>
2026-05-23 02:23:37 +0200

    Use bootstrap

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,21 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
     ? '/countdown/'
-    : '/'
+    : '/',
+  build: {
+    outDir: './dist'
+  },
+  // Silence Sass deprecation warnings
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          'color-functions',
+          'if-function',
+          'import',
+          'global-builtin'
+        ]
+      }
+    }
+  }
 })
